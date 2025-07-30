2078a70a876586dab2a3c9ac5a7567bd20281793
Akash <Akash.patel.ap88020@gmail.com>
2025-07-30 16:55:57 +0530

    feat: Add Home, Shop, and About pages with routing using react-router-dom

---
+++ frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Collection from './pages/Collectiion'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Product from './pages/Product'
+import Cart from './pages/Cart'
+import Login from './pages/Login'
+import PlaceOrder from './pages/PlaceOrder'
+import Orders from './pages/Orders'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div className='px-4 md:px-[7vw] sm:px-[5vw] lg:px-[9vw]'>
+      < Navbar />
+      <Routes>
+        <Route path='/' element={< Home />} />
+        <Route path='/collection' element={< Collection />} />
+        <Route path='/about' element={< About />} />
+        <Route path='/contact' element={< Contact />} />
+        <Route path='/product/:productId' element={< Product />} />
+        <Route path='/cart' element={< Cart />} />
+        <Route path='/Login' element={< Login />} />
+        <Route path='/place-order' element={< PlaceOrder />} />
+        <Route path='/Orders' element={< Orders />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/Collectiion.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Collectiion = () => {
+  return (
+    <div>Collectiion</div>
+  )
+}
+
+export default Collectiion
