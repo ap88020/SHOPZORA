74268f69a0da5fa36b0cbb7f6057ee6063fb901d
Akash <Akash.patel.ap88020@gmail.com>
2025-08-27 20:00:11 +0530

    created userOrder , allOrders for admin panel controller and also fetch on the fronted

--- backend/controllers/ordersControllers.js
+++ backend/controllers/ordersControllers.js
@@ -1,69 +1,74 @@
-import ordersModel from '../models/ordersModel.js'
-import userModel from '../models/userModel.js';
+import orderModel from "../models/ordersModel.js";
+import ordersModel from "../models/ordersModel.js";
+import userModel from "../models/userModel.js";
 
 // Placing orders using COD Method
-const placeOrder = async (req,res) => {
-    try {
+const placeOrder = async (req, res) => {
+  try {
+    const { userId, items, amount, address } = req.body;
 
-        const { userId , items , amount , address } = req.body;
+    const orderData = {
+      userId,
+      items,
+      amount,
+      address,
+      paymentMethod: "COD",
+      payment: false,
+      date: Date.now(),
+    };
 
-        const orderData = {
-            userId,
-            items,
-            amount,
-            address,
-            paymentMethod:"COD",
-            payment:false, 
-            date: Date.now()
-        }
+    const newOrder = new ordersModel(orderData);
 
-        const newOrder = new ordersModel(orderData);
+    await newOrder.save();
 
-        await newOrder.save();
+    await userModel.findByIdAndUpdate(userId, { cartData: {} });
 
-        await userModel.findByIdAndUpdate(userId,{cartData:{}});
-
-        res.json({success:true , message :"Order Placed"})
-
-    } catch (error) {
-        console.log(error);
-        res.json({success:false , message:error.message})
-    }
-}
+    res.json({ success: true, message: "Order Placed" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // Placing orders using Stripe Method
-const placeOrderStripe = async (req,res) => {
-    
-}
+const placeOrderStripe = async (req, res) => {};
 
 // Placing orders using Stripe Method
-const placeOrderRozarpay = async (req,res) => {
-
-}
+const placeOrderRozarpay = async (req, res) => {};
 
 // All Orders data for Admin Panel
-const allOrders = async (req,res) => {
-
-}
+const allOrders = async (req, res) => {
+  try {
+    const orders = await orderModel.find({});
+    res.json({ success: true, orders });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // userOrder data for Frontend
-const userOrders = async (req,res) => {
-    try {
-        const { userId } = req.body;
+const userOrders = async (req, res) => {
+  try {
+    const { userId } = req.body;
 
-        const orders = await ordersModel.find({userId});
+    const orders = await ordersModel.find({ userId });
 
-        res.json({success:true, orders})
-        
-    } catch (error) {
-        console.log(error)
-        res.json({success:false , message:error.message})
-    }
-}
+    res.json({ success: true, orders });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // Update Orders Status From Admin Panel
-const updateStatus = async (req,res) => {
-    
-}
+const updateStatus = async (req, res) => {};
 
-export {placeOrder ,placeOrderStripe ,placeOrderRozarpay ,allOrders ,userOrders ,updateStatus};
+export {
+  placeOrder,
+  placeOrderStripe,
+  placeOrderRozarpay,
+  allOrders,
+  userOrders,
+  updateStatus,
+};
